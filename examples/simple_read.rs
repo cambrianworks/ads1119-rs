a0e903b7bf670dcbe7c5ac99013e71b165156817
Clint Frederickson <clint@cambrianworks.com>
2023-10-03 12:12:01 -0600

    split apart driver and example

---
+++ examples/simple_read.rs
@@ -0,0 +1,108 @@
+use std::time::Duration;
+
+use linux_embedded_hal::I2cdev;
+
+use ads1119::{Ads1119, MuxFlags, STATUS_CONV_RDY, single_ended_rdata_to_scaled_voltage};
+
+
+/// This main is an example of using the library to read single-ended data
+/// off each of the four inputs.
+fn main() {
+    let dev = I2cdev::new("/dev/i2c-7").unwrap();
+    let mut driver = Ads1119::new(dev, 0x40);
+
+    // Reset the device to a known state (default)
+    let _ = driver.reset().unwrap();
+    println!("Reset called.");
+
+    // default config shoul be 0x0
+    let config = driver.read_config().unwrap();
+    println!("Read config value: {:X}", config);
+
+    // default status should not have the MSBit set
+    let status = driver.read_status().unwrap();
+    println!("Read status value: {:#010b}", status);
+
+    // loop forever
+    loop {
+        // read each input on the ADS1119
+        for mux in [
+            MuxFlags::AN0_SINGLE_ENDED,
+            MuxFlags::AN1_SINGLE_ENDED,
+            MuxFlags::AN2_SINGLE_ENDED,
+            MuxFlags::AN3_SINGLE_ENDED,
+        ] {
+            // write the config to set the input we want. Leave other fields unset (default)
+            // println!("writing config...");
+            driver.write_config(mux).unwrap();
+
+            // read back the config as a sanity check
+            let config = driver.read_config().unwrap();
+            println!("Read config value: {:X}", config);
+
+            // start a "one-shot" converstion on the selected input
+            let _ = driver.start_sync().unwrap();
+
+            // wait until the status register tells us there is data to read
+            loop {
+                let status = driver.read_status().unwrap();
+                // println!("CURRENT STATUS: {:#010b} ", status);
+                if status & STATUS_CONV_RDY != 0 {
+                    println!("--> Data is READY: {:#010b} ", status);
+                    break;
+                }
+                std::thread::sleep(Duration::from_millis(10))
+            }
+
+            // read the conversion data
+            let raw_value = driver.read_data().unwrap();
+            println!(
+                "[{:X}] Read (conv) value: {:.5}V",
+                mux,
+                // convert the data to a voltage
+                single_ended_rdata_to_scaled_voltage(raw_value)
+            );
+
+            // wait a bit before reading the next input
+            std::thread::sleep(Duration::from_millis(1000));
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    const EPS: f32 = 0.0001;
+    const V_MAX: f32 = 2.048;
+
+    use super::*;
+    #[test]
+    fn rdata_to_voltage_0() {
+        let data = [0b0000_0000, 0b0000_0000];
+        assert_eq!(single_ended_rdata_to_scaled_voltage(&data), 0.0f32);
+    }
+
+    #[test]
+    fn rdata_to_voltage_max_pos() {
+        let data = [0b0111_1111, 0b1111_1111];
+        assert!(single_ended_rdata_to_scaled_voltage(&data) - 2.048f32 < EPS);
+    }
+
+    #[test]
+    fn rdata_to_voltage_lt_max() {
+        let data = [0b0111_1111, 0b1111_1110];
+        assert!(single_ended_rdata_to_scaled_voltage(&data) < V_MAX);
+    }
+
+    #[test]
+    fn rdata_to_voltage_max_neg() {
+        let data = [0b1000_0000, 0b0000_0000];
+        assert!(dbg!((single_ended_rdata_to_scaled_voltage(&data) - -2.048f32).abs()) < EPS);
+    }
+
+    #[test]
+    fn rdata_to_voltage_gt_max_neg() {
+        let data = [0b1000_0000, 0b0000_0001];
+        assert!(dbg!(single_ended_rdata_to_scaled_voltage(&data)) > -V_MAX);
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,155 @@
+use std::time::Duration;
+
+use embedded_hal::i2c::I2c;
+use linux_embedded_hal::I2cdev;
+
+pub struct Ads1119<I2C> {
+    i2c: I2C,
+    // I2C address
+    address: u8,
+}
+
+impl<I2C> Ads1119<I2C>
+where
+    I2C: I2c,
+{
+    pub fn new(i2c: I2C, i2c_address: u8) -> Self {
+        Ads1119 {
+            i2c,
+            address: i2c_address,
+        }
+    }
+
+    /// Read the config register
+    /// See 8.5.3.6 RREG
+    ///
+    /// Returns the config register as a u8.
+    /// See 8.6.1
+    ///
+    /// Means of returned byte
+    /// | Bit 7 | Bit 6 | Bit 5 | Bit 4 | Bit 3 | Bit 2 | Bit 1     | Bit 0 |
+    /// | MUX Selection         | GAIN  | Data Rate     | Conv Mode | VREF  |
+    ///
+    /// See [MuxFlags]
+    pub fn read_config(&mut self) -> Result<u8, I2C::Error> {
+        let mut read_buffer = [0];
+        self.i2c
+            .write_read(
+                self.address,
+                // set the config register bit
+                &[CmdFlags::RREG | RegFlags::CONFIG],
+                &mut read_buffer,
+            )
+            .and(Ok(read_buffer[0]))
+    }
+
+    /// Write the config register with the given value. See [read_config] for u8 structure.
+    ///
+    /// See [MuxFlags]
+    pub fn write_config(&mut self, value: u8) -> Result<(), I2C::Error> {
+        self.i2c
+            .write(self.address, &[CmdFlags::WREG | RegFlags::CONFIG, value]) //A0
+                                                                              // .write(self.address, &[CmdFlags::WREG | RegFlags::CONFIG, 0xA0]) //A2
+    }
+
+    /// Read the status register.
+    ///
+    /// See 8.5.3.6 RREG
+    /// See 8.6.1 and 8.6.2.2
+    ///
+    /// The only bit that matters is the MSB. If set, a new converion is ready to be read
+    /// with [read_data]. If it isn't set, the application should wait and check the status register again.
+    pub fn read_status(&mut self) -> Result<u8, I2C::Error> {
+        let mut read_buffer = [0];
+        self.i2c
+            .write_read(
+                self.address,
+                &[CmdFlags::RREG | RegFlags::STATUS],
+                &mut read_buffer,
+            )
+            .and(Ok(read_buffer[0]))
+    }
+
+    /// In single-shot conversion mode (the only one currently supported), this starts a conversion.
+    /// Before reading a result, use [read_status] to check the the conversion has finished.
+    /// See 8.5.3.3
+    pub fn start_sync(&mut self) -> Result<(), I2C::Error> {
+        self.i2c.write(self.address, &[CmdFlags::START_SYNC])
+    }
+
+    /// Resets the device to a default state.
+    /// See 8.5.3.2
+    pub fn reset(&mut self) -> Result<(), I2C::Error> {
+        self.i2c.write(self.address, &[CmdFlags::RESET])
+    }
+
+    /// Reads data from the currently selected input.
+    /// The u16 result is encoded as Two's Complement.
+    ///
+    /// Currently, the library has only been used to read positive, single-ended values.
+    ///
+    /// See 8.5.3.5 RDATA
+    /// See 8.5.2 Data Format
+     pub fn read_data(&mut self) -> Result<u16, I2C::Error> {
+        let mut read_buffer = [0u8, 0u8];
+        self.i2c
+            .write_read(self.address, &[CmdFlags::RDATA], &mut read_buffer)
+            .and(Ok(u16::from_be_bytes(read_buffer)))
+    }
+}
+
+/// Interpret the raw data read from one of the inputs as a voltage
+/// Currently, this function assumes the reference voltage is the internal 2.048V source
+/// See 8.3.3 Voltage Reference
+///     8.5.2 Data Format
+pub fn single_ended_rdata_to_scaled_voltage(raw_data: u16) -> f32 {
+    // Check the sign bit (Bit 15)
+    let unscaled_voltage = if (raw_data & 0x8000) != 0 {
+        // // Two's complement conversion for negative values
+        raw_data.wrapping_neg() as i16
+    } else {
+        raw_data as i16
+    };
+
+    // Positive value, directly scale based on the ADS1119's configuration
+    // In this case, the reference voltage is 2.048V
+    const REFERENCE_VOLTAGE: f32 = 2.048;
+
+    // Scale the voltage to the desired range (e.g., 0V to 2.048V)
+    let scaled_voltage = (unscaled_voltage as f32 / 0x7FFF as f32) * REFERENCE_VOLTAGE;
+    scaled_voltage
+}
+/// Command Flags
+/// See 8.5.3
+pub struct CmdFlags;
+impl CmdFlags {
+    pub const RESET: u8 = 0b0000_0110;
+    pub const START_SYNC: u8 = 0b0000_1000;
+    pub const POWER_DOWN: u8 = 0b0000_0010;
+    pub const RDATA: u8 = 0b0001_0000;
+    pub const RREG: u8 = 0b0010_0000;
+    pub const WREG: u8 = 0b0100_0000;
+}
+
+/// Input Mux flags
+/// See 8.6.2.1 Configuration Register
+/// See 8.3.1 Multiplexer
+pub struct MuxFlags;
+impl MuxFlags {
+    pub const AN0_SINGLE_ENDED: u8 = 0b0110_0000;
+    pub const AN1_SINGLE_ENDED: u8 = 0b1000_0000;
+    pub const AN2_SINGLE_ENDED: u8 = 0b1010_0000;
+    pub const AN3_SINGLE_ENDED: u8 = 0b1100_0000;
+}
+
+/// Register flags meant to be to combined with eh RREG command to select
+/// the correct register
+/// See 8.5.3 (RREG)
+/// See 8.6.1 - Table 8 (Register column)
+pub struct RegFlags;
+impl RegFlags {
+    pub const CONFIG: u8 = 0b0000_0000;
+    pub const STATUS: u8 = 0b0000_0100;
+}
+
+pub const STATUS_CONV_RDY: u8 = 0b1000_0000;
